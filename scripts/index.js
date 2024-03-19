617e732dc0d4b548cff4678b16b16e31468610f2
techoceanic <techoceanic.tech@gmail.com>
2024-03-19 15:05:23 +0530

    Updated the Site with calendly Setup

--- scripts/index.js
+++ scripts/index.js
@@ -34,8 +34,8 @@
 // cards.forEach(initOverlayCard);
 // document.body.addEventListener("pointermove", applyOverlayMask);
 
-// Hamburger
 
+// Hamburger
 function toggleHam() {
   var menu_items = document.getElementById("menu_items");
 
@@ -45,3 +45,55 @@ function toggleHam() {
     menu_items.style.display = "none";
   }
 }
+
+
+function toggleService() {
+  console.log("In the Toggle Services Section");
+  var item_btn_service = document.getElementById("nav_services");
+
+  if (item_btn_service.style.display == "none" || item_btn_service.style.display == "") {
+    item_btn_service.style.display = "flex";
+  } else {
+    item_btn_service.style.display = "none";
+  }
+}
+
+
+function toggleCalendly() {
+  // console.log("In the Toggle Services Section");
+  var calendly_meeting = document.getElementById("calendly_meeting");
+
+  if (calendly_meeting.style.display == "none" || calendly_meeting.style.display == "") {
+    calendly_meeting.style.display = "block";
+  } else {
+    calendly_meeting.style.display = "none";
+  }
+}
+
+
+document.querySelector("main").addEventListener(('click'), () => {
+  console.log("Clicked on the Body");
+  var nav_services = document.getElementById("nav_services");
+  var calendly_meeting = document.getElementById("calendly_meeting");
+
+  if (nav_services.style.display == "none" || nav_services.style.display == "") {
+    // nav_services.style.display = "flex";
+    var menu_items = document.getElementById("menu_items");
+    if (menu_items.style.display == "none" || menu_items.style.display == "") {
+      console.log("");
+    } else {
+      menu_items.style.display = "none";
+    }
+    console.log("");
+  } else {
+    nav_services.style.display = "none";
+  }
+
+  if (calendly_meeting.style.display == "none" || calendly_meeting.style.display == "") {
+    console.log("");
+  } else {
+    calendly_meeting.style.display = "none";
+  }
+
+
+})
